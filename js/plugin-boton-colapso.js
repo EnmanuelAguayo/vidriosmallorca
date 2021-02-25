7525488330cb9a7df55748fe425b73d5df45a283
EnmanuelAguayo <enmanuelruizdiaz@hotmail.com>
2021-02-24 22:26:08 -0300

    version 24 feb 2021 cambios varios

--- js/plugin-boton-colapso.js
+++
@@ -1,5 +0,0 @@
-
-  document.getElementsByClassName('sidenav-trigger')[0].addEventListener('click', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
